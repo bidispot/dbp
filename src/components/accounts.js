d7029b152351964d605a2334b9603f67aa783287
Sebastien Schaeffler <bidisoft@gmail.com>
2016-09-13 16:38:01 +0200

    Draft for query and list pages

--- src/components/accounts.js
+++ src/components/accounts.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import NavBar from '../containers/nav_bar';
-
+import Query from '../containers/accounts_query'
+import List from '../containers/accounts_list'
 // Base styling
 import "../style/style.css";
 
@@ -9,7 +10,8 @@ export default class Accounts extends Component {
     return (
       <div>
         <NavBar />
-        Accounts
+        <Query />
+        <List />
       </div>
     );
   }
--- src/containers/nav_bar.js
+++ src/containers/nav_bar.js
@@ -17,7 +17,7 @@ export default class NavBar extends Component {
         <Navbar.Collapse>
           <Nav pullRight>
             <NavItem eventKey={1} href="#">Notifications <Badge>0</Badge></NavItem>
-            <NavItem eventKey={2} href="#">Logged in as: <span className="teal">Jeff Bezos (hl666) </span></NavItem>
+            <NavItem eventKey={2} href="#">Logged in as: <span className="teal">Jeff Bezos (hl666)</span></NavItem>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
