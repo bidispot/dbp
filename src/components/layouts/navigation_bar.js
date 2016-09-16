57a010ea269f7f33c6a04ef032d157a5a97095c4
Sebastien Schaeffler <bidisoft@gmail.com>
2016-09-16 08:57:34 +0200

    Fix DOM issue while embedded a div or p inside a Navbar.Text

--- src/components/layouts/navigation_bar.js
+++ src/components/layouts/navigation_bar.js
@@ -7,149 +7,149 @@ export default class NavBar extends Component {
   render() {
     return (
       <div>
-      <Navbar fluid>
-        <Navbar.Header>
-          <Navbar.Brand>
-            <Link to="/">
-              <span className="teal">Digital Business Platform</span>
-            </Link>
-          </Navbar.Brand>
-          <Navbar.Toggle />
-        </Navbar.Header>
-        <Navbar.Collapse>
-          <Nav pullRight>
+        <Navbar fluid>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <Link to="/">
+                <span className="teal">Digital Business Platform</span>
+              </Link>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
             <Navbar.Text>
-              Hello John Cooper <span className="welcome-date">22 September 2016</span>
+              <span className="welcome">Hello </span><span className="teal">John Cooper </span><span className="welcome-date">22 September 2016</span>
             </Navbar.Text>
             <div className="clock-div"><Clock /></div>
-            <NavDropdown eventKey={1} title={<span  className="teal"><Glyphicon glyph="envelope" />
-              <Badge className="navbar-badge navbar-badge-messages">3</Badge></span>} id="messages">
-              <MenuItem eventKey="1">
-                <div> <strong>John Smith</strong> <span className="pull-right text-muted"> <em>Yesterday</em> </span> </div>
-                <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Pellentesque eleifend...</div>
-              </MenuItem>
-              <MenuItem divider />
-              <MenuItem eventKey="2">
-                <div> <strong>John Smith</strong> <span className="pull-right text-muted"> <em>Yesterday</em> </span> </div>
-                <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Pellentesque eleifend...</div>
-              </MenuItem>
-              <MenuItem divider />
-              <MenuItem eventKey="3">
-                <div> <strong>John Smith</strong> <span className="pull-right text-muted"> <em>Yesterday</em> </span> </div>
-                <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Pellentesque eleifend...</div>
-              </MenuItem>
-              <MenuItem divider />
-              <MenuItem eventKey="4">
-                <strong>Read All Messages</strong> <span className="pull-right"><Glyphicon glyph="triangle-right" /></span>
-              </MenuItem>
-            </NavDropdown>
-            <NavDropdown eventKey={2} title={<span className="navbar-tasks"><Glyphicon glyph="tasks" />
-              <Badge className="navbar-badge navbar-badge-tasks">3</Badge></span> } id="tasks">
-              <MenuItem eventKey="1" style={ { width: 300 } }>
-                <div>
-                  <p> <strong>Task 1</strong> <span className="pull-right text-muted">80% Complete</span> </p>
+            <Nav pullRight>
+              <NavDropdown eventKey={1} title={<span  className="teal"><Glyphicon glyph="envelope" />
+                <Badge className="navbar-badge navbar-badge-messages">3</Badge></span>} id="messages">
+                <MenuItem eventKey="1">
+                  <div> <strong>John Smith</strong> <span className="pull-right text-muted"> <em>Yesterday</em> </span> </div>
+                  <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Pellentesque eleifend...</div>
+                </MenuItem>
+                <MenuItem divider />
+                <MenuItem eventKey="2">
+                  <div> <strong>John Smith</strong> <span className="pull-right text-muted"> <em>Yesterday</em> </span> </div>
+                  <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Pellentesque eleifend...</div>
+                </MenuItem>
+                <MenuItem divider />
+                <MenuItem eventKey="3">
+                  <div> <strong>John Smith</strong> <span className="pull-right text-muted"> <em>Yesterday</em> </span> </div>
+                  <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Pellentesque eleifend...</div>
+                </MenuItem>
+                <MenuItem divider />
+                <MenuItem eventKey="4">
+                  <strong>Read All Messages</strong> <span className="pull-right"><Glyphicon glyph="triangle-right" /></span>
+                </MenuItem>
+              </NavDropdown>
+              <NavDropdown eventKey={2} title={<span className="navbar-tasks"><Glyphicon glyph="tasks" />
+                <Badge className="navbar-badge navbar-badge-tasks">3</Badge></span> } id="tasks">
+                <MenuItem eventKey="1" style={ { width: 300 } }>
                   <div>
-                    <ProgressBar striped bsStyle="success" now={80} />
+                    <p> <strong>Task 1</strong> <span className="pull-right text-muted">80% Complete</span> </p>
+                    <div>
+                      <ProgressBar striped bsStyle="success" now={80} />
+                    </div>
                   </div>
-                </div>
-              </MenuItem>
-              <MenuItem divider />
-              <MenuItem eventKey="2">
-                <div>
-                  <p> <strong>Task 2</strong> <span className="pull-right text-muted">20% Complete</span> </p>
+                </MenuItem>
+                <MenuItem divider />
+                <MenuItem eventKey="2">
                   <div>
-                    <ProgressBar striped bsStyle="warning" now={20} />
+                    <p> <strong>Task 2</strong> <span className="pull-right text-muted">20% Complete</span> </p>
+                    <div>
+                      <ProgressBar striped bsStyle="warning" now={20} />
+                    </div>
                   </div>
-                </div>
-              </MenuItem>
-              <MenuItem divider />
-              <MenuItem eventKey="3">
-                <div>
-                  <p> <strong>Task 3</strong> <span className="pull-right text-muted">60% Complete</span> </p>
+                </MenuItem>
+                <MenuItem divider />
+                <MenuItem eventKey="3">
                   <div>
-                    <ProgressBar striped bsStyle="info" now={60} />
+                    <p> <strong>Task 3</strong> <span className="pull-right text-muted">60% Complete</span> </p>
+                    <div>
+                      <ProgressBar striped bsStyle="info" now={60} />
+                    </div>
                   </div>
-                </div>
-              </MenuItem>
-              <MenuItem divider />
-              <MenuItem eventKey="4">
-                <strong>See All Tasks</strong> <span className="pull-right"><Glyphicon glyph="triangle-right" /></span>
-              </MenuItem>
-            </NavDropdown>
-            <NavDropdown eventKey={3} title={<span  className="navbar-notif"><Glyphicon glyph="bell" />
-              <Badge className="navbar-badge navbar-badge-notif">4</Badge></span>} id="notifications">
-              <MenuItem eventKey="1" style={ { width: 300 } }>
-                <div> <span className="teal"><Glyphicon bsStyle="info" glyph="comment" /></span> New Comment <span className="pull-right text-muted small">4 minutes ago</span> </div>
-              </MenuItem>
-              <MenuItem divider />
-              <MenuItem eventKey="2">
-                <div> <span  className="orange"> <Glyphicon glyph="tasks" /> </span> New Task <span className="pull-right text-muted small">12 minutes ago</span> </div>
-              </MenuItem>
-              <MenuItem divider />
-              <MenuItem eventKey="3">
-                <div> <span className="green"> <Glyphicon glyph="envelope" /> </span> Message Sent <span className="pull-right text-muted small">24 minutes ago</span> </div>
-              </MenuItem>
-              <MenuItem divider />
-              <MenuItem eventKey="4" style={ { width: 300 } }>
-                <div> <span className="teal"><Glyphicon bsStyle="info" glyph="comment" /></span> New Comment <span className="pull-right text-muted small">4 minutes ago</span> </div>
-              </MenuItem>
-              <MenuItem divider />
-              <MenuItem eventKey="5">
-                <strong>See All Alerts</strong> <span className="pull-right"><Glyphicon glyph="triangle-right" /></span>
-              </MenuItem>
-            </NavDropdown>
-            <NavDropdown eventKey={4} title={<span  className="teal"><Glyphicon glyph="user" /></span>} id="profile">
-              <MenuItem eventKey="1">
-                <Glyphicon glyph="user" /> User Profile
-              </MenuItem>
-              <MenuItem eventKey="2">
-                <Glyphicon glyph="cog" /> Settings
-              </MenuItem>
-              <MenuItem divider />
-              <MenuItem eventKey="4">
-                <Glyphicon glyph="log-out" /> Logout
-              </MenuItem>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
+                </MenuItem>
+                <MenuItem divider />
+                <MenuItem eventKey="4">
+                  <strong>See All Tasks</strong> <span className="pull-right"><Glyphicon glyph="triangle-right" /></span>
+                </MenuItem>
+              </NavDropdown>
+              <NavDropdown eventKey={3} title={<span  className="navbar-notif"><Glyphicon glyph="bell" />
+                <Badge className="navbar-badge navbar-badge-notif">4</Badge></span>} id="notifications">
+                <MenuItem eventKey="1" style={ { width: 300 } }>
+                  <div> <span className="teal"><Glyphicon bsStyle="info" glyph="comment" /></span> New Comment <span className="pull-right text-muted small">4 minutes ago</span> </div>
+                </MenuItem>
+                <MenuItem divider />
+                <MenuItem eventKey="2">
+                  <div> <span  className="orange"> <Glyphicon glyph="tasks" /> </span> New Task <span className="pull-right text-muted small">12 minutes ago</span> </div>
+                </MenuItem>
+                <MenuItem divider />
+                <MenuItem eventKey="3">
+                  <div> <span className="green"> <Glyphicon glyph="envelope" /> </span> Message Sent <span className="pull-right text-muted small">24 minutes ago</span> </div>
+                </MenuItem>
+                <MenuItem divider />
+                <MenuItem eventKey="4" style={ { width: 300 } }>
+                  <div> <span className="teal"><Glyphicon bsStyle="info" glyph="comment" /></span> New Comment <span className="pull-right text-muted small">4 minutes ago</span> </div>
+                </MenuItem>
+                <MenuItem divider />
+                <MenuItem eventKey="5">
+                  <strong>See All Alerts</strong> <span className="pull-right"><Glyphicon glyph="triangle-right" /></span>
+                </MenuItem>
+              </NavDropdown>
+              <NavDropdown eventKey={4} title={<span  className="teal"><Glyphicon glyph="user" /></span>} id="profile">
+                <MenuItem eventKey="1">
+                  <Glyphicon glyph="user" /> User Profile
+                </MenuItem>
+                <MenuItem eventKey="2">
+                  <Glyphicon glyph="cog" /> Settings
+                </MenuItem>
+                <MenuItem divider />
+                <MenuItem eventKey="4">
+                  <Glyphicon glyph="log-out" /> Logout
+                </MenuItem>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
 
-        <div className="navbar-default sidebar" style={ { 'marginLeft': '-20px' } } role="navigation">
-          <div className="sidebar-nav navbar-collapse">
-            <ul className="nav in" id="side-menu">
-              <li className="sidebar-search">
-                <div className="input-group custom-search-form">
-                  <input type="text" className="form-control" placeholder="Search..." />
-                  <span className="input-group-btn">
-                    <button className="btn btn-default" type="button">
-                      <Glyphicon glyph="search" />
-                    </button>
-                  </span>
-                </div>
-              </li>
-              <li>
-                <Link to="/"><Glyphicon glyph="dashboard" /> &nbsp; Dashboard</Link>
-              </li>
-              <li>
-                <Link to="/balances" className="balances_primary_color"><Glyphicon glyph="usd" /> &nbsp; Cash balances</Link>
-              </li>
-              <li>
-                <Link to="/indicators" className="indicators_primary_color"><Glyphicon glyph="time" /> &nbsp; Indicators</Link>
-              </li>
-              <li className="accounts_primary_color">
-                <Link to="/accounts"><Glyphicon glyph="user" /> &nbsp; Accounts</Link>
-              </li>
-              <li>
-                <Link to="/ref_data" className="ref_data_primary_color"><Glyphicon glyph="stats" /> &nbsp; Reference Data</Link>
-              </li>
-              <li>
-                <Link to="/"> &nbsp; About</Link>
-              </li>
-            </ul>
+          <div className="navbar-default sidebar" style={ { 'marginLeft': '-20px' } } role="navigation">
+            <div className="sidebar-nav navbar-collapse">
+              <ul className="nav in" id="side-menu">
+                <li className="sidebar-search">
+                  <div className="input-group custom-search-form">
+                    <input type="text" className="form-control" placeholder="Search..." />
+                    <span className="input-group-btn">
+                      <button className="btn btn-default" type="button">
+                        <Glyphicon glyph="search" />
+                      </button>
+                    </span>
+                  </div>
+                </li>
+                <li>
+                  <Link to="/"><Glyphicon glyph="dashboard" /> &nbsp; Dashboard</Link>
+                </li>
+                <li>
+                  <Link to="/balances" className="balances_primary_color"><Glyphicon glyph="usd" /> &nbsp; Cash balances</Link>
+                </li>
+                <li>
+                  <Link to="/indicators" className="indicators_primary_color"><Glyphicon glyph="time" /> &nbsp; Indicators</Link>
+                </li>
+                <li className="accounts_primary_color">
+                  <Link to="/accounts"><Glyphicon glyph="user" /> &nbsp; Accounts</Link>
+                </li>
+                <li>
+                  <Link to="/ref_data" className="ref_data_primary_color"><Glyphicon glyph="stats" /> &nbsp; Reference Data</Link>
+                </li>
+                <li>
+                  <Link to="/"> &nbsp; About</Link>
+                </li>
+              </ul>
+            </div>
           </div>
-        </div>
-      </Navbar>
+        </Navbar>
 
-      {this.props.children}
+        {this.props.children}
 
       </div>
     );
