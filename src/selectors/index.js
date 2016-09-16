12d2966fbc26d281b1be234fd53bd581cf929591
Alexandros Skaliotis <skaliotis@gmail.com>
2016-09-16 10:03:54 +0200

    Linked balance list and balance query partially with state

--- src/selectors/index.js
+++ src/selectors/index.js
@@ -4,6 +4,8 @@ const getCashBalances = (state) => state.database.get('balances');
 
 const getCashBalanceResultIds = (state) => state.balances.queryResults;
 
+export const getCashBalancesQueryParameters = (state) => state.balances.queryParameters;
+
 // Memoized selector that receives the state as argument and returns the list of
 // Balance objects representing the Cash Balances query results.
 export const getCashBalancesQueryResults = createSelector(
