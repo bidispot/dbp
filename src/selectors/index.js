e9ae6d3de054e4a14d7dbc869859fb485c0e79db
Alexandros Skaliotis <skaliotis@gmail.com>
2016-09-17 23:56:55 +0200

    Fix waiting indicator

--- src/selectors/index.js
+++ src/selectors/index.js
@@ -6,6 +6,8 @@ const getCashBalanceResultIds = (state) => state.balances.queryResults;
 
 export const getCashBalancesQueryParameters = (state) => state.balances.queryParameters;
 
+export const getIsCashBalancesQuerying = (state) => state.balances.isQuerying;
+
 // Memoized selector that receives the state as argument and returns the list of
 // Balance objects representing the Cash Balances query results.
 export const getCashBalancesQueryResults = createSelector(
@@ -20,6 +22,8 @@ const getAccounts = (state) => state.database.get('accounts');
 const getAccountsResultIds = (state) => state.accounts.queryResults;
 
 export const getAccountsQueryParameters = (state) => state.accounts.queryParameters;
+
+export const getIsAccountsQuerying = (state) => state.accounts.isQuerying;
 
 // Memoized selector that receives the state as argument and returns the list of
 // Account objects representing the Accounts query results.
